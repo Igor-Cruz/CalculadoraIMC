060019bd23a0f7e11f104b5589ec88b2190e948f
Igor-Cruz <igorcruz.it@outlook.com>
2021-07-16 18:39:31 -0300

    Finalizando

--- imc.js
+++ imc.js
@@ -1,15 +1,46 @@
 // Selecionando pelos id
-const nome = document.getElementById('nome')
-const altura = document.getElementById('altura')
-const peso = document.getElementById('peso')
 const calcular = document.getElementById('calcular')
-const resultado = document.getElementById('resultado')
+
+//Lógica  IMC
+function imc() {
+  const nome = document.getElementById('nome').value
+  const altura = document.getElementById('altura').value
+  const peso = document.getElementById('peso').value
+  const resultado = document.getElementById('resultado')
+  //Validação de dados
+  if (nome !== '' && altura !== '' && peso !== '') {
+    const resultadoIMC = (peso / (altura * altura)).toFixed(2)
+    let classificacao = ''
+    let grau = ''
+
+    if (resultadoIMC < 18.5) {
+      classificacao = 'Magreza'
+      grau = 0
+    } else if (resultadoIMC < 24.9) {
+      classificacao = 'Normal'
+      grau = 0
+    } else if (resultadoIMC < 29.9) {
+      classificacao = 'Sobrepeso'
+      grau = 1
+    } else if (resultadoIMC < 39.9) {
+      classificacao = 'Obesidade'
+      grau = 2
+    } else {
+      classificacao = 'Obesidade Grave'
+      grau = 3
+    }
+
+    //Mensagem que aparece na Div
+    resultado.textContent = `${nome}, o seu IMC é ${resultadoIMC},você está com a classificação ${classificacao} de grau ${grau}  `
+  } else {
+    resultado.textContent = 'Preencha todos os campos !!! '
+  }
+}
 
 //Evento de click
 calcular.addEventListener('click', imc)
 
-//Lógica  IMC
-function resultado() {
+/*
   const imc = peso / (altura * 2)
   let classificacao = ''
   let grau = ''
@@ -30,10 +61,10 @@ function resultado() {
     grau = 3
   }
   // return resultado()
-  resultado.textContent = `Olá ${nome}, o seu IMC tem o resultado de ${imc}, a sua  é classificação  de ${classificacao} e grau ${grau}`
+ 
 }
 
-/*
+
 const nome = document.querySelector('#nome')
 const peso = document.querySelector('#peso')
 const altura = document.querySelector('#altura')
