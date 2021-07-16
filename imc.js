a6b6c6a58200005eaa50f60bbc71e5be157533bb
Igor-Cruz <igorcruz.it@outlook.com>
2021-07-16 17:48:36 -0300

    Selecionando os campos através do id no js.

--- imc.js
+++ imc.js
@@ -1,19 +1,45 @@
-// Calculo imc
+// Selecionando pelos id
+const nome = document.getElementById('#nome')
+const altura = document.getElementById('#altura')
+const peso = document.getElementById('#peso')
+const calcular = document.getElementById('#calcular')
+const resultado = document.getElementById('#resultado')
 
-var nome = prompt('Qual o seu nome ? ')
-var peso = parseFloat(prompt('Qual o seu peso ? '))
-var altura = parseFloat(prompt('Qual a sua altura ?'))
-var alturaFinal = altura * 2
-var imc = peso / alturaFinal
+/*
+const nome = document.querySelector('#nome')
+const peso = document.querySelector('#peso')
+const altura = document.querySelector('#altura')
+*/
 
-if (imc < 18.5) {
-  console.log('Magreza')
-} else if (imc <= 24.9) {
-  console.log('Normal')
-} else if (imc <= 29.9) {
-  console.log('Sobrepeso')
-} else if (imc <= 39.9) {
-  console.log('Obesidade')
-} else {
-  console.log('Obesidade grave')
+// button calcular
+document.querySelector('#calcular')
+
+//resultado
+document.querySelector('#resultado')
+
+resultado = `Olá ${nome}, o seu IMC tem o resultado de ${imc}, a sua  é classificação  de ${classificacao} e grau ${grau}`
+
+//Lógica  IMC
+function resultado() {
+  const imc = peso / (altura * 2)
+  let classificacao = ''
+  let grau = ''
+  if (imc < 18.5) {
+    classificacao = 'Magreza'
+    grau = 0
+  } else if (imc <= 24.9) {
+    classificacao = 'Normal'
+    grau = 0
+  } else if (imc <= 29.9) {
+    classificacao = 'Sobrepeso'
+    grau = 1
+  } else if (imc <= 39.9) {
+    classificacao = 'Obesidade'
+    grau = 2
+  } else {
+    classificacao = 'Obesidade Grave'
+    grau = 3
+  }
+  // return resultado()
+  resultado.textContent = `Olá ${nome}, o seu IMC tem o resultado de ${imc}, a sua  é classificação  de ${classificacao} e grau ${grau}`
 }
